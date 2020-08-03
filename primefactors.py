b3c0d9b5366436809554ee7b8b5908e9ea7da185
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-03 15:16:29 -0400

    changes

--- primefactors.py
+++ primefactors.py
@@ -1,26 +1,15 @@
 
 def getPrimes(num):
     primes = []
+    div = 2
 
-    while num != 0:
-        if num % 2 == 0:
-            primes.append(2)
-            num = num - (num % 2)
-        elif num % 3 == 0:
-            primes.append(3)
-            num = num - (num % 3)
-        elif num % 5 == 0:
-            primes.append(5)
-            num = num - (num % 5)
-        elif num % 7 == 0:
-            primes.append(7)
-            num = num - (num % 7)
-    
-    if num > 0:
-        p_list = [num, 1]
-        return p_list
-    else:
-        return primes
+    while num >= div:
+        if num % div == 0:
+            primes.append(div)
+            num = num / div
+        else:
+            div += 1
+    return primes
     
 
 def main():
