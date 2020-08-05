e8695c6cc49102785e48950482fcd172104fc478
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-04 21:02:57 -0400

    new

---
+++ strSort.py
@@ -0,0 +1,24 @@
+import string
+
+def strSort(expr):
+    list1 = expr.split(' ')
+    sorted_list = sorted(list1, key=str.casefold)
+    out = ""
+
+    for word in sorted_list:
+        out += word + " "
+
+    return out.strip()
+    
+
+def main():
+    exp = input("Please enter an expression: ")
+    print (f'{strSort(exp)}')
+
+if __name__ == '__main__':
+    main()
+
+
+'''
+
+OFFICIAL SOLUTION
