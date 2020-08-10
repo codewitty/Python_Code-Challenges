39d510c276faff961ea09d9445ea28144a6f175d
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-10 19:28:54 -0400

    changes

--- alarm.py
+++ alarm.py
@@ -1,14 +1,13 @@
-from playsound import playsound
+import sched
+import time
+import winsound as ws
 
-def set_alarm(time, sound, message):
+def set_alarm(alarm_time, wav_file, message):
+    s = sched.scheduler(time.time, time.sleep)
+    s.enterabs(alarm_time, 1, print, argument=(message,))
+    s.enterabs(alarm_time, 1, ws.PlaySound, argument=(wav_file, ws.SND_FILENAME))
+    print('Alarm set for', time.asctime(time.localtime(alarm_time)))
+    s.run()
 
-    playsound(sound)
-    print(f'{message}')
-
-
-
-def main():
-    set_alarm(25, "upbeat.mp3", "Wake UP!!!!")
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__':    
+    set_alarm(time.time()+1, 'upbeat.mp3', 'Wake up!')
