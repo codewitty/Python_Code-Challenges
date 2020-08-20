58c1f4f6f9f69b9f074282d7e343f31119bc4050
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-19 23:44:27 -0400

    final iteration for now. bug fixes needed'

---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib
+
+SENDER_EMAIL = 'born2codetech@gmail.com'
+SENDER_PASSWORD = ''
+
+def send_email(receiver_email, subject, body):
+    message = (f'Subject: {subject}n\n{body}')
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.sendmail(SENDER_EMAIL, receiver_email, message)
+
+def main():
+    sub = 'This is a test email to check if it works. AND IT WORKED!!!!'
+    body = 'IT WORKED'
+    rec = 'gomesjoshua@gmail.com'
+    send_email(rec, sub, body)
+
+if __name__ == '__main__':
+    main()
