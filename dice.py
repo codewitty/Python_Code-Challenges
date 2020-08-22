3413f355c379ddfb2512ebecb05586fa5586c72f
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-21 23:31:03 -0400

    new challenge

--- dice.py
+++ dice.py
@@ -2,16 +2,26 @@ import random
 
 def roll_dice(*args):
     d_dict = {}
-    sums = 0
-    for num in range(0, 1000000):
+    for num in range(0, 1000):
+        sums = 0
+        temp_dict = {}
+        for arg in args:
+            for num in range(1, arg):
+                if num in d_dict:
+                    d_dict[num] += 1
+                else:
+                    d_dict[num] = 1
+        for key, value in temp_dict.items():
+            d_dict[value] = 0
+
         for arg in args:
             sums += (random.randint(1, arg))
-        if sums in d_dict:
-            d_dict[sums] += 1
-        else:
+        #if sums in d_dict:
+            #d_dict[sums] += 1
+        #else:
             d_dict[sums] = 1
     for key, value in d_dict.items():
-        print(f'{key} : {(value/10000)} \n')
+        print(f'{key} : {(value/10)} \n')
     
 
 def main():
