f26a5eadbd2ef7c43cee5c87bccb6caca78414bc
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-22 00:03:16 -0400

    final iteration

--- dice.py
+++ dice.py
@@ -1,28 +1,21 @@
 import random
 
-def roll_dice(*args):
+def roll_dice(*args, trials = 1000000):
     d_dict = {}
-    for num in range(0, 1000):
-        sums = 0
-        temp_dict = {}
-        for arg in args:
-            for num in range(1, arg):
-                if num in d_dict:
-                    d_dict[num] += 1
-                else:
-                    d_dict[num] = 1
-        for key, value in temp_dict.items():
-            d_dict[value] = 0
+    temp_dict = {}
 
+    for roll in range(trials):
+        sums = 0
         for arg in args:
             sums += (random.randint(1, arg))
-        #if sums in d_dict:
-            #d_dict[sums] += 1
-        #else:
+        if sums in d_dict:
+            d_dict[sums] += 1
+        else:
             d_dict[sums] = 1
-    for key, value in d_dict.items():
-        print(f'{key} : {(value/10)} \n')
-    
+
+    print (f'\nOUTCOME\tPROBABILITY')
+    for outcome in range (len(args), sum(args) + 1):
+        print(f'{outcome}\t{d_dict[outcome]*100/trials}')
 
 def main():
     roll_dice(4,6,6)
