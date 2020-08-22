3d22d3229f1490413bc55d1be10f66cf158cb67b
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-21 22:53:44 -0400

    new challenge

---
+++ dice.py
@@ -0,0 +1,18 @@
+import string
+
+def prob(*args):
+    
+    expr = expr.lower()
+    expr = expr.replace(" ", "")
+    expr = expr.translate(str.maketrans('', '', string.punctuation))
+    pal = expr[::-1]
+
+    return expr == pal
+    
+
+def main():
+    exp = input("Please enter an expression: ")
+    print (f'{palindrome(exp)}')
+
+if __name__ == '__main__':
+    main()
