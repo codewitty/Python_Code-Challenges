3136b1b8e0c00349329f19a0b651e89794f63470
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-21 23:13:42 -0400

    new challenge

--- dice.py
+++ dice.py
@@ -1,18 +1,21 @@
-import string
+import random
 
-def prob(*args):
-    
-    expr = expr.lower()
-    expr = expr.replace(" ", "")
-    expr = expr.translate(str.maketrans('', '', string.punctuation))
-    pal = expr[::-1]
-
-    return expr == pal
+def roll_dice(*args):
+    d_dict = {}
+    sums = 0
+    for num in range(0, 1000000):
+        for arg in args:
+            sums += (random.randint(1, arg))
+        if sums in d_dict:
+            d_dict[sums] += 1
+        else:
+            d_dict[sums] = 1
+    for key, value in d_dict.items():
+        print(f'{key} : {(value/10000)} \n')
     
 
 def main():
-    exp = input("Please enter an expression: ")
-    print (f'{palindrome(exp)}')
+    roll_dice(4,6,6)
 
 if __name__ == '__main__':
     main()
