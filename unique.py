894f237ca277d9c47f8ed050de835adee5832c3c
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-22 23:44:36 -0400

    final iteration with recommended solution

--- unique.py
+++ unique.py
@@ -1,5 +1,5 @@
-import string
 import re
+from collections import Counter
 
 def take(n, iterable):
     "Return first n items of the iterable as a list"
@@ -7,7 +7,36 @@ def take(n, iterable):
 
 def unique(file_name):
     with open(file_name, 'r') as file:
-        data = file.read().replace('\n', '')
+        data = re.findall(r"[0-9a-zA-Z-']+", file.read())
+        data = [words.upper() for words in data]
+        print (f'Total word count: {len(data)}')
+
+    dict_save = Counter() 
+
+    for word in data:
+        dict_save[word] += 1
+
+    sort_orders = sorted(dict_save.items(), key=lambda x: x[1], reverse=True)
+    n_items = sort_orders[0:20]
+
+    for group in n_items:
+        print(f'{group[0]} appears {group[1]} times')
+    #Another way to count appearences:
+    """
+    for word in dict_save.most_common[20]:
+        print(f'{word[0]} appears {word[1]} times')
+    """
+
+    file.close()
+
+
+def main():
+    exp = "works.txt"
+    print (f'{unique(exp)}')
+
+if __name__ == '__main__':
+    main()
+    """
     data = data.lower()
     # Create string
     remove = string.punctuation
@@ -21,31 +50,4 @@ def unique(file_name):
     result = re.sub(pattern, " ", data)
     # Split text string and add to list
     out = result.split()
-    dict_save = {}
-    count = 0
-
-    for word in out:
-        if word in dict_save.keys():
-            dict_save[word] += 1
-        else:
-            dict_save[word] = 1
-        count += 1
-
-    print (f'Total word count: {count}')
-
-    sort_orders = sorted(dict_save.items(), key=lambda x: x[1], reverse=True)
-
-    n_items = sort_orders[0:20]
-
-    for group in n_items:
-        print(f'{group[0]} appears {group[1]} times')
-
-    file.close()
-
-
-def main():
-    exp = "works.txt"
-    print (f'{unique(exp)}')
-
-if __name__ == '__main__':
-    main()
+    """
