c0458b59c104ca7e976d8bf10706a63116dbabb6
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-22 23:23:02 -0400

    final iteration

--- unique.py
+++ unique.py
@@ -1,6 +1,10 @@
 import string
 import re
 
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+
 def unique(file_name):
     with open(file_name, 'r') as file:
         data = file.read().replace('\n', '')
@@ -15,9 +19,29 @@ def unique(file_name):
     pattern = r"[{}]".format(remove)
     # Strip punctuation from the input
     result = re.sub(pattern, " ", data)
+    # Split text string and add to list
+    out = result.split()
+    dict_save = {}
+    count = 0
+
+    for word in out:
+        if word in dict_save.keys():
+            dict_save[word] += 1
+        else:
+            dict_save[word] = 1
+        count += 1
+
+    print (f'Total word count: {count}')
+
+    sort_orders = sorted(dict_save.items(), key=lambda x: x[1], reverse=True)
+
+    n_items = sort_orders[0:20]
+
+    for group in n_items:
+        print(f'{group[0]} appears {group[1]} times')
+
     file.close()
 
-    return result
 
 def main():
     exp = "works.txt"
