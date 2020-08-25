654d1dfcb6149f6bc8c3968408649b18b14f2518
Joshua Gomes <gomesjoshua@gmail.com>
2020-08-25 00:04:28 -0400

    Final iteration

--- password.py
+++ password.py
@@ -1,15 +1,26 @@
 import string
+import random
 
 def generate_password(length):
     passphrases = {}
+    password = ""
     with open("passcode.txt") as myfile:
         for line in myfile:
             passkey, phrase = line.partition("\t")[::2]
-            passphrases[passkey.strip()] = phrase.strip("\n")
-    
+            passphrases[passkey] = phrase.strip("\n")
+    #print(passphrases)
+    for num in range(5):
+        code = ""
+        for roll in range(5):
+            code += str(random.randint(1, 6))
+        print(code)
+        password += passphrases[code] + " "
+
+    return password.rstrip()
+ 
 
 def main():
-    generate_password(5)
+    print(f'Your password is: {generate_password(5)}')
 
 if __name__ == '__main__':
     main()
